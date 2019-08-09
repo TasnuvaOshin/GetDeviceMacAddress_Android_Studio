2acdf084b71dea58746fb41fb08229ec86c3753e
Tasnuva Tabassum oshin <tasnuva.oshin12@gmail.com>
2019-08-09 18:07:22 +0600

    Create DeviceClass.java

---
+++ DeviceClass.java
@@ -0,0 +1,36 @@
+package apps.searchme.searchmeapplication.Util;
+
+import java.net.NetworkInterface;
+import java.util.Collections;
+import java.util.List;
+
+public class  DeviceClass {
+
+    public static String getMacAddress() {
+        try {
+            String interfaceName = "wlan0";
+            List<NetworkInterface> interfaces = Collections.list(NetworkInterface.getNetworkInterfaces());
+            for (NetworkInterface intf : interfaces) {
+                if (!intf.getName().equalsIgnoreCase(interfaceName)) {
+                    continue;
+                }
+
+                byte[] mac = intf.getHardwareAddress();
+                if (mac == null) {
+                    return "";
+                }
+
+                StringBuilder buf = new StringBuilder();
+                for (byte aMac : mac) {
+                    buf.append(String.format("%02X:", aMac));
+                }
+                if (buf.length() > 0) {
+                    buf.deleteCharAt(buf.length() - 1);
+                }
+                return buf.toString();
+            }
+        } catch (Exception ex) {
+        } // for now eat exceptions
+        return "";
+    }
+}
