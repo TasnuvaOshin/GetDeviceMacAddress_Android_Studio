8d11a94d21feda575a009912a5268ffdc39e2e7e
Tasnuva Tabassum oshin <tasnuva.oshin12@gmail.com>
2019-08-09 18:08:24 +0600

    Create Fragment.java

---
+++ Fragment.java
@@ -0,0 +1,123 @@
+For Fragment You Have to Use this :
+
+package apps.searchme.searchmeapplication.Auth;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+
+import java.net.NetworkInterface;
+import java.util.Collections;
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.legacy.app.FragmentCompat;
+import apps.searchme.searchmeapplication.Home.HomeFragment;
+import apps.searchme.searchmeapplication.MainActivity;
+import apps.searchme.searchmeapplication.R;
+import apps.searchme.searchmeapplication.Util.DeviceClass;
+
+
+public class RegFragment extends Fragment {
+    private static final int MY_PERMISSIONS_REQUEST_LOCATION = 99 ;
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        View view = inflater.inflate(R.layout.fragment_reg, container, false);
+
+        if(checkLocationPermission()) {
+            Toast.makeText(getActivity(), "" +DeviceClass.getMacAddress(), Toast.LENGTH_SHORT).show();
+        }
+
+
+        return view;
+    }
+
+ 
+
+    //permission check for the activity
+    public boolean checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(getActivity(),
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                new AlertDialog.Builder(getActivity())
+                        .setTitle("Permission")
+                        .setMessage("Please Share/on Your Location")
+                        .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                //Prompt the user once explanation has been shown
+                                requestPermissions( new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                        MY_PERMISSIONS_REQUEST_LOCATION);
+
+                            }
+                        })
+                        .create()
+                        .show();
+
+
+            } else {
+//                // No explanation needed, we can request the permission.
+//                ActivityCompat.requestPermissions(getActivity(),
+//                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+//                        MY_PERMISSIONS_REQUEST_LOCATION);
+                requestPermissions( new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+            }
+
+            return false;
+        } else {
+
+            return true;
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        switch (requestCode){
+
+
+            case MY_PERMISSIONS_REQUEST_LOCATION:{
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(getActivity(), "Permission Granted", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(getActivity(),MainActivity.class));
+
+
+                }
+
+            }
+
+        }
+    }
+}
